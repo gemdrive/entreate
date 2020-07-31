95a523c3f2135f50900d7ce5235fa37cd1e42665
Anders Pitman <tapitman11@gmail.com>
2020-07-31 14:01:19 -0600

    Add CodeMirror text editor

--- entreate.js
+++ entreate.js
@@ -286,10 +286,38 @@ function EntryEditor(entryUrl, text, meta, allTags) {
     tags = e.detail.tags;
   });
 
-  const textInput = el('textarea');
-  textInput.classList.add('entreate-entry-editor__text-input');
-  dom.appendChild(MarginBox(textInput));
-  textInput.value = text;
+  let fontSize = 16;
+  const decreaseFontBtn = el('button', {
+    onclick: () => {
+      fontSize -= 1;
+      textContainer.style['font-size'] = fontSize + 'px';
+    },
+  });
+  decreaseFontBtn.innerText = "- Font size";
+  dom.appendChild(decreaseFontBtn);
+  const increaseFontBtn = el('button', {
+    onclick: () => {
+      fontSize += 1;
+      textContainer.style['font-size'] = fontSize + 'px';
+    },
+  });
+  increaseFontBtn.innerText = "+ Font size";
+  dom.appendChild(increaseFontBtn);
+
+  const textContainer = el('div');
+  textContainer.classList.add('entreate-entry-editor__text');
+  dom.appendChild(textContainer);
+
+  const editor = CodeMirror(textContainer, {
+    lineNumbers: true,
+    value: text,
+    //viewportMargin: Infinity,
+  });
+
+  // See https://stackoverflow.com/a/19970695/943814
+  setTimeout(() => {
+    editor.refresh();
+  }, 0);
 
 
   return {
