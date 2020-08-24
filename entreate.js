6902320d7efc4201d50a5abc2e85fcdb6214ad50
Anders Pitman <tapitman11@gmail.com>
2020-08-24 13:47:16 -0600

    Move EntryEditor to separate file

--- entreate.js
+++ entreate.js
@@ -3,7 +3,8 @@ import {
   idToPath,
 } from './utils.js';
 import { publishAllEntries } from './publish.js';
-import { TagEditor, TagList } from './tag_editor.js';
+import { EntryEditor } from './entry_editor.js';
+import { TagList } from './tag_editor.js';
 
 
 function Entreate(driveUri, path, token) {
@@ -268,90 +269,6 @@ function EntryListItem(entryUrl, token) {
   });
 
   return dom;
-}
-
-
-function EntryEditor(entryUrl, text, meta, allTags) {
-  const dom = el('div');
-  dom.classList.add('entreate-entry-editor');
-
-  const homeButton = el('button', {
-    onclick: (e) => {
-      dom.dispatchEvent(new CustomEvent('close', {
-        bubbles: true,
-      }));
-    },
-  });
-  homeButton.innerText = 'Home';
-  dom.appendChild(homeButton);
-
-  const saveButton = el('button', {
-    onclick: (e) => {
-      dom.dispatchEvent(new CustomEvent('save', {
-        bubbles: true,
-        detail: {
-          entryUrl,
-          text: editor.getValue(),
-          meta: Object.assign(meta, {
-            title: titleInput.getValue() ? titleInput.getValue() : 'Untitled',
-            tags,
-          }),
-        },
-      }));
-    },
-  });
-  saveButton.classList.add('entreate-button', 'entreate-button-confirm');
-  saveButton.innerText = 'Save';
-  dom.appendChild(saveButton);
-
-  const titleInput = ValueInput('Title', meta.title);
-  dom.appendChild(MarginBox(titleInput.dom));
-
-  let tags = meta.tags;
-  const tagEditor = TagEditor(allTags, meta.tags);
-  dom.appendChild(MarginBox(tagEditor.dom));
-  tagEditor.dom.addEventListener('tags-changed', (e) => {
-    tags = e.detail.tags;
-  });
-
-  let fontSize = 16;
-  const decreaseFontBtn = el('button', {
-    onclick: () => {
-      fontSize -= 1;
-      textContainer.style['font-size'] = fontSize + 'px';
-    },
-  });
-  decreaseFontBtn.innerText = "- Font size";
-  dom.appendChild(decreaseFontBtn);
-  const increaseFontBtn = el('button', {
-    onclick: () => {
-      fontSize += 1;
-      textContainer.style['font-size'] = fontSize + 'px';
-    },
-  });
-  increaseFontBtn.innerText = "+ Font size";
-  dom.appendChild(increaseFontBtn);
-
-  const textContainer = el('div');
-  textContainer.classList.add('entreate-entry-editor__text');
-  dom.appendChild(textContainer);
-
-  const editor = CodeMirror(textContainer, {
-    lineNumbers: true,
-    value: text,
-    //viewportMargin: Infinity,
-  });
-
-  // See https://stackoverflow.com/a/19970695/943814
-  setTimeout(() => {
-    editor.refresh();
-  }, 0);
-
-
-  return {
-    dom,
-    updateTags: tagEditor.updateTags,
-  };
 }
 
 
---
+++ entry_editor.js
@@ -0,0 +1,89 @@
+import {
+  el, parseGemData, ValueInput, MarginBox, entryIterator, naturalSorter,
+  idToPath,
+} from './utils.js';
+import { TagEditor } from './tag_editor.js';
+
+
+export function EntryEditor(entryUrl, text, meta, allTags) {
+  const dom = el('div');
+  dom.classList.add('entreate-entry-editor');
+
+  const homeButton = el('button', {
+    onclick: (e) => {
+      dom.dispatchEvent(new CustomEvent('close', {
+        bubbles: true,
+      }));
+    },
+  });
+  homeButton.innerText = 'Home';
+  dom.appendChild(homeButton);
+
+  const saveButton = el('button', {
+    onclick: (e) => {
+      dom.dispatchEvent(new CustomEvent('save', {
+        bubbles: true,
+        detail: {
+          entryUrl,
+          text: editor.getValue(),
+          meta: Object.assign(meta, {
+            title: titleInput.getValue() ? titleInput.getValue() : 'Untitled',
+            tags,
+          }),
+        },
+      }));
+    },
+  });
+  saveButton.classList.add('entreate-button', 'entreate-button-confirm');
+  saveButton.innerText = 'Save';
+  dom.appendChild(saveButton);
+
+  const titleInput = ValueInput('Title', meta.title);
+  dom.appendChild(MarginBox(titleInput.dom));
+
+  let tags = meta.tags;
+  const tagEditor = TagEditor(allTags, meta.tags);
+  dom.appendChild(MarginBox(tagEditor.dom));
+  tagEditor.dom.addEventListener('tags-changed', (e) => {
+    tags = e.detail.tags;
+  });
+
+  let fontSize = 16;
+  const decreaseFontBtn = el('button', {
+    onclick: () => {
+      fontSize -= 1;
+      textContainer.style['font-size'] = fontSize + 'px';
+    },
+  });
+  decreaseFontBtn.innerText = "- Font size";
+  dom.appendChild(decreaseFontBtn);
+  const increaseFontBtn = el('button', {
+    onclick: () => {
+      fontSize += 1;
+      textContainer.style['font-size'] = fontSize + 'px';
+    },
+  });
+  increaseFontBtn.innerText = "+ Font size";
+  dom.appendChild(increaseFontBtn);
+
+  const textContainer = el('div');
+  textContainer.classList.add('entreate-entry-editor__text');
+  dom.appendChild(textContainer);
+
+  const editor = CodeMirror(textContainer, {
+    lineNumbers: true,
+    value: text,
+    //viewportMargin: Infinity,
+  });
+
+  // See https://stackoverflow.com/a/19970695/943814
+  setTimeout(() => {
+    editor.refresh();
+  }, 0);
+
+
+  return {
+    dom,
+    updateTags: tagEditor.updateTags,
+  };
+}
